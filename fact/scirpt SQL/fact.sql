b1d9229b00bff8c857e6e50494c3ef5e1c3c05b1
IIulixesII <157491405+IIulixesII@users.noreply.github.com>
2025-06-23 11:56:49 -0300

    Base sql

--- fact/scirpt SQL/fact.sql
+++ fact/scirpt SQL/fact.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-05-2025 a las 06:06:13
+-- Tiempo de generación: 23-06-2025 a las 16:48:12
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
@@ -29,23 +29,27 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `cliente` (
   `id` int(11) NOT NULL,
-  `Nombre` varchar(100) NOT NULL,
-  `Telefono` varchar(20) NOT NULL,
-  `Fecha_Nacimiento` date NOT NULL,
-  `Saldo` decimal(10,2) NOT NULL,
-  `Consumo_luz` int(11) NOT NULL,
-  `Dni` int(15) NOT NULL
+  `nombre` varchar(100) NOT NULL,
+  `apellido` varchar(100) NOT NULL,
+  `telefono` varchar(20) NOT NULL,
+  `fecha_nacimiento` date NOT NULL,
+  `saldo` decimal(10,2) NOT NULL,
+  `consumo_luz` int(11) NOT NULL,
+  `dni` int(15) NOT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `cliente`
 --
 
-INSERT INTO `cliente` (`id`, `Nombre`, `Telefono`, `Fecha_Nacimiento`, `Saldo`, `Consumo_luz`, `Dni`) VALUES
-(1, 'ulises', '2954326436', '2025-05-05', 32122.00, 2223, 45109702),
-(7, 'ticiano', '21314', '2025-05-01', 145.00, 1234, 123),
-(8, 'Dario', '2131243', '2025-05-14', 50090.00, 214124, 222),
-(9, 'Catalina ', '2954326436', '2025-05-13', 124124.00, 123124412, 777);
+INSERT INTO `cliente` (`id`, `nombre`, `apellido`, `telefono`, `fecha_nacimiento`, `saldo`, `consumo_luz`, `dni`, `usuario_id`) VALUES
+(1, 'Ulises', 'Martínez', '2954326436', '1998-05-05', 32122.00, 2223, 45109702, 3),
+(2, 'Ticiano', 'Gómez', '21314', '2000-01-01', 145.00, 1234, 12345678, 4),
+(3, 'Dario', 'Pérez', '2131243', '1995-12-12', 50090.00, 214124, 22333444, 5),
+(4, 'Catalina', 'López', '2954326436', '1997-07-13', 124124.00, 123124412, 44777888, 6),
+(15, 'wqqwe', 'wqeqwe', '3123', '0132-12-23', 0.00, 0, 123, 17),
+(16, 'fac', 'fa', '02954326436', '0444-03-21', 0.00, 0, 123, 18),
+(17, 'asada', 'awsdwad', '02954326436', '0001-03-12', 0.00, 0, 123, 19);
 
 -- --------------------------------------------------------
 
@@ -64,12 +68,29 @@ CREATE TABLE `factura` (
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `soporte`
+--
+
+CREATE TABLE `soporte` (
+  `id_soporte` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `telefono` varchar(20) DEFAULT NULL,
+  `dni` varchar(20) DEFAULT NULL,
+  `mensaje` text NOT NULL,
+  `fecha` timestamp NULL DEFAULT current_timestamp(),
+  `valoracion` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `turno`
 --
 
 CREATE TABLE `turno` (
   `id` int(11) NOT NULL,
-  `Nombre` varchar(100) DEFAULT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
   `numero` varchar(10) DEFAULT NULL,
   `estado` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -78,11 +99,42 @@ CREATE TABLE `turno` (
 -- Volcado de datos para la tabla `turno`
 --
 
-INSERT INTO `turno` (`id`, `Nombre`, `numero`, `estado`) VALUES
-(3, 'Dario', '2', 'sinatender'),
-(4, 'Catalina ', '3', 'sinatender'),
-(5, 'ulises', '4', 'sinatender'),
-(6, 'ticiano', '5', 'sinatender');
+INSERT INTO `turno` (`id`, `nombre`, `numero`, `estado`) VALUES
+(1, 'Dario', '2', 'sinatender'),
+(2, 'Catalina', '3', 'sinatender'),
+(3, 'Ulises', '4', 'sinatender'),
+(4, 'Ticiano', '5', 'sinatender');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
+  `nombreUsuario` varchar(100) DEFAULT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `rol` varchar(20) NOT NULL,
+  `activo` tinyint(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `nombreUsuario`, `email`, `password`, `rol`, `activo`) VALUES
+(1, 'Admin1', 'a@a.com', '$2a$07$asxx54ahjppf45sd87a5auJRR6foEJ7ynpjisKtbiKJbvJsoQ8VPS', 'admin', 1),
+(2, 'Cajero1', 'cajero@a.com', '$2a$07$asxx54ahjppf45sd87a5auJRR6foEJ7ynpjisKtbiKJbvJsoQ8VPS', 'cajero', 1),
+(3, 'Ulises123', 'ulises@a.com', '$2a$07$asxx54ahjppf45sd87a5auJRR6foEJ7ynpjisKtbiKJbvJsoQ8VPS', 'cliente', 1),
+(4, 'Ticiano456', 'ticiano@a.com', '$2a$07$asxx54ahjppf45sd87a5auJRR6foEJ7ynpjisKtbiKJbvJsoQ8VPS', 'cliente', 1),
+(5, 'Dario789', 'dario@a.com', '$2a$07$asxx54ahjppf45sd87a5auJRR6foEJ7ynpjisKtbiKJbvJsoQ8VPS', 'cliente', 1),
+(6, 'Cata001', 'catalina@a.com', '$2a$07$asxx54ahjppf45sd87a5auJRR6foEJ7ynpjisKtbiKJbvJsoQ8VPS', 'cliente', 1),
+(14, 'ulises', 'uli@uli', '$2y$10$asxx54ahjppf45sd87a5auJRR6foEJ7ynpjisKtbiKJbvJsoQ8VPS', 'cajero', 1),
+(17, 'qweqe', 'qwaq@dasd', '$2y$10$aquUrt/OB3Xe/A.ckjzTqOdHLFsNhl32bE7UY0dTyRq3MPMtbkrmi', 'cliente', 1),
+(18, 'facu', 'fa@fa', '$2y$10$.Rce2MuivG441e1G1cSz3.g/UTBW9eQ85Oi4qlmGoD9bTvk5F4q4u', 'cliente', 1),
+(19, 'zxas', 'b@b', '$2y$10$k/TBPR7eZUkcrcrlQNWKD.Z1NuKriNEYoMaD6Zlydfo3OBdaRAMES', 'cliente', 1);
 
 --
 -- Índices para tablas volcadas
@@ -92,7 +144,8 @@ INSERT INTO `turno` (`id`, `Nombre`, `numero`, `estado`) VALUES
 -- Indices de la tabla `cliente`
 --
 ALTER TABLE `cliente`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id` (`usuario_id`);
 
 --
 -- Indices de la tabla `factura`
@@ -102,9 +155,21 @@ ALTER TABLE `factura`
   ADD KEY `cliente_id` (`cliente_id`);
 
 --
+-- Indices de la tabla `soporte`
+--
+ALTER TABLE `soporte`
+  ADD PRIMARY KEY (`id_soporte`);
+
+--
 -- Indices de la tabla `turno`
 --
 ALTER TABLE `turno`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -115,7 +180,7 @@ ALTER TABLE `turno`
 -- AUTO_INCREMENT de la tabla `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT de la tabla `factura`
@@ -124,20 +189,38 @@ ALTER TABLE `factura`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT de la tabla `soporte`
+--
+ALTER TABLE `soporte`
+  MODIFY `id_soporte` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `turno`
 --
 ALTER TABLE `turno`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
+-- Filtros para la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD CONSTRAINT `fk_cliente_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`);
+
+--
 -- Filtros para la tabla `factura`
 --
 ALTER TABLE `factura`
-  ADD CONSTRAINT `factura_ibfk_1` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`id`);
+  ADD CONSTRAINT `fk_factura_cliente` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
